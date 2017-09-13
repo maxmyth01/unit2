6036f2cd03b8a1bbefbd9d23164adf50469faeb1
maxmyth01 <maxmyth01@gmail.com>
2017-09-13 13:33:10 -0400

    Updated from Brython Server: 9/13/2017 1:33:09 PM

--- ifDemo.py
+++ ifDemo.py
@@ -2,4 +2,15 @@
 #9-13-17
 #ifDemo.py -- introduction to if statements
 
+num = float(input('Enter a number: '))
 
+if num > 0:
+    print(num,'is postitive')
+    
+elif num < 0:
+    print(num,'is negitive')
+    
+elif num == 0:
+    print(num,'is zero')
+
+    
