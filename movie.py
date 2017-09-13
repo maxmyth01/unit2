e09ad37d2b4f2b8c7e71af148656b62117c8e6a5
maxmyth01 <maxmyth01@gmail.com>
2017-09-13 13:55:44 -0400

    Updated from Brython Server: 9/13/2017 1:55:43 PM

--- movie.py
+++ movie.py
@@ -1,1 +1,15 @@
+#Max Low
+#9-13-17
+#movie.py -- give an age and type of movie you can watch
+
+age = float(input('Enter your age: '))
+
+if age <=8:
+    print('You can watch G movies')
+elif age <=13:
+    print('You can watch PG movies')
+elif age <=17:
+    print('You can watch PG-13 movies')
+else:
+    print('You can watch R movies')
 
