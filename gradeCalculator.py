9c0699f55fe3bef84650c0c659736466792a2e79
maxmyth01 <maxmyth01@gmail.com>
2017-09-13 13:48:07 -0400

    Updated from Brython Server: 9/13/2017 1:48:06 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,23 @@
+#Max Low
+#9-13-17
+#gradeCalculator -- convert a numerical grade to a letter
 
+grade = float(input('Input your grade: '))
+
+if grade > 100:
+    print('Show off')
+
+elif grade >=90:
+    print('You got an A')
+    
+elif grade >=80:
+    print('You got a B')
+
+elif grade >=70:
+    print('You got a C')
+
+elif grade >=60:
+    print('You got a D')
+    
+else:
+    print('You got a failing grade')
