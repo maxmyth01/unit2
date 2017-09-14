92ad715861dc506f4b745ef14294871a4fefb806
maxmyth01 <maxmyth01@gmail.com>
2017-09-14 14:50:46 -0400

    Updated from Brython Server: 9/14/2017 2:50:46 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -2,4 +2,8 @@
 #9-14-17
 #fortuneTeller.py -- give a fortune based on color and number
 
+color = input('Pick red or blue') 
+num = int(input('Pick a number from 1-4: '))
 
+if color == 'red':
+    print('yes')
