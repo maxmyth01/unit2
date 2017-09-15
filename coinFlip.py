50ff048fbdf04c61bddfa8482213072f2521c65b
maxmyth01 <maxmyth01@gmail.com>
2017-09-15 14:52:39 -0400

    Updated from Brython Server: 9/15/2017 2:52:38 PM

--- coinFlip.py
+++ coinFlip.py
@@ -1,1 +1,12 @@
+#Max Low
+#9-15-17
+#coinFlip.py -- print heads or tails
 
+from random import randint
+
+num = randint(0,1)
+if num == 0:
+    print('heads')
+if num == 1:
+    print('tails')
+    
