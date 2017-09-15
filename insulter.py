2a963defec865c30682f36c05a3e040fe43d23c8
maxmyth01 <maxmyth01@gmail.com>
2017-09-15 14:46:11 -0400

    Updated from Brython Server: 9/15/2017 2:46:09 PM

--- insulter.py
+++ insulter.py
@@ -8,4 +8,13 @@ insultNum = randint(1,5)
 
 input('Enter your name')
 
-print(insultNum)
+if insultNum == 1:
+    print('You Suck at life')
+if insultNum == 2:
+    print('')
+if insultNum == 3:
+    print('You Suck at life')
+if insultNum == 4:
+    print('You Suck at life')
+if insultNum == 5:
+    print('You Suck at life')
