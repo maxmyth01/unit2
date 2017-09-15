b9965726d906805ec783e0cfc025a331d8922bb1
maxmyth01 <maxmyth01@gmail.com>
2017-09-15 14:26:20 -0400

    Updated from Brython Server: 9/15/2017 2:26:19 PM

--- warmup3.py
+++ warmup3.py
@@ -1,1 +1,20 @@
+#Max Low
+#9-15-17
+# warmup3.py -- divisible by 3, 2, both, neither
+
+num = int(input('Enter a number: '))
+
+if num % 2 == 0:
+    if num % 3 == 0:
+        print(num, 'is divisible by 2 and 3')
+    elif:
+        print(num, 'is divisible by 2')
+
+elif num % 3 == 0:
+    if num % 3 ==0:
+        print(num, 'is divisible by 3')
+        
+else:
+    print(num,'is not divisible by 2 or 3')
+        
 
