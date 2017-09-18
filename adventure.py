4957373dd52be670c0d47f507d947f63609346bf
maxmyth01 <maxmyth01@gmail.com>
2017-09-18 14:50:06 -0400

    Updated from Brython Server: 9/18/2017 2:50:05 PM

--- adventure.py
+++ adventure.py
@@ -6,7 +6,7 @@ mountain = str(input('You arrive at the foot of a mountain. The climb looks to b
 if mountain = yes:
     cave = str(input('Good choice, you begin up the mountain as and boulder falls in to the spot you were standing moments ago . . . As you continue higher up you see a bright light coming from a cave up a head. Do you enter the cave?(yes or no)'))
     if cave = yes:
-        print()
+        print('As you explore deeper into the cave walls seem to glow, and the tempature increases but you carry on. Until . . . you being to  . . . pass out from the heat . . . as you fall the the ground, you spot a river of lava deep in the cave, ever increasing in size. You know the end is neigh, as you go unconcious.(GAMEOVER)')
     elif cave = no:
         print('You contine higher up the mountain, as a collum of fire and lava erupts from the cave. You luckly survive and continue higher to the summit. Good for You. You reached the top.(YOU WIN)  )
     else:
