b78a06f0c26197572ec531065ceec242b294d1da
maxmyth01 <maxmyth01@gmail.com>
2017-09-18 14:43:40 -0400

    Updated from Brython Server: 9/18/2017 2:43:40 PM

--- adventure.py
+++ adventure.py
@@ -1,3 +1,17 @@
 #Max Low
 #9-15-17
 #adventure.py -- two choices
+
+mountain = str(input('You arrive at the foot of a mountain. The climb looks to be hard and gruling and will probably kill you. Do you dare climb the Mountain?(yes or no)'))
+if mountain = yes:
+    cave = str(input('Good choice, you begin up the mountain as and boulder falls in to the spot you were standing moments ago . . . As you continue higher up you see a bright light coming from a cave up a head. Do you enter the cave?(yes or no)'))
+    if cave = yes:
+        print()
+    elif cave = no:
+        print('You contine higher up the mountain, as a collum of fire and lava erupts from the cave. You luckly survive and continue higher to the summit. Good for You. You reached the top.(YOU WIN)  )
+    else:
+        print('INVAILD INPUT, GAMEOVER')
+elif mountain = no:
+    print('Have a nice day, as you begin to walk away from the mountain the ground shakes as an boulder falls twards you, GAMEOVER')
+else:
+    print('INVAILD INPUT, GAMEOVER')
