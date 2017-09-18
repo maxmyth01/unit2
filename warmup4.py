f9cdc31d8239eb9551514fe2700781ad12611358
maxmyth01 <maxmyth01@gmail.com>
2017-09-18 14:21:58 -0400

    Updated from Brython Server: 9/18/2017 2:21:58 PM

--- warmup4.py
+++ warmup4.py
@@ -1,1 +1,12 @@
+#Max Low
+#9-18-17
+#warmup.py - buzz game on 7's or divisible by 7
 
+buzz = int(input('Enter a number'))
+if buzz // 7 == 0:
+    print('buzz')
+buzz = str(buzz)
+if '7' in buzz:
+    print('buzz')
+else:
+    print(buzz)
