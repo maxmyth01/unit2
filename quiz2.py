f5512b8ca6b5f78e75e9b7d418a5af6bac08730a
maxmyth01 <maxmyth01@gmail.com>
2017-09-25 14:35:43 -0400

    Updated from Brython Server: 9/25/2017 2:35:42 PM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,23 @@
+#Max Low
+#9-25-17
+#quiz2.py -- numbers , bigger smaller same, divisible by 3, product and correct person
 
+numone = int(input('Enter a number: '))
+numtwo = int(input('Enter a 2nd number: '))
+
+"""if numone > numtwo:
+    print('The first number is bigger')
+elif numtwo > numone:
+    print('The second number is bigger')
+else:
+    print('The numbers are the same')
+"""
+    
+if numone % 3 == 0 and numtwo % == 0:
+    print('They are both divisible by 3')
+elif numone % 3 == 0:
+    print('Only the first number is divisible by three')
+elif numtwo % 3 == 0:
+    print('Only the second number is divisible by three')
+else:
+    print('Neither number is divisible by 3')
