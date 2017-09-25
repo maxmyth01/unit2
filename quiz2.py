e558d318f0538a88a89b1c0444c37f1a24107aee
maxmyth01 <maxmyth01@gmail.com>
2017-09-25 14:43:48 -0400

    Updated from Brython Server: 9/25/2017 2:43:47 PM

--- quiz2.py
+++ quiz2.py
@@ -5,15 +5,15 @@
 numone = int(input('Enter a number: '))
 numtwo = int(input('Enter a 2nd number: '))
 
-"""if numone > numtwo:
+if numone > numtwo:
     print('The first number is bigger')
 elif numtwo > numone:
     print('The second number is bigger')
 else:
     print('The numbers are the same')
-"""
-    
-if numone % 3 == 0 and numtwo % == 0:
+
+   
+if numone % 3 == 0 and numtwo % 3 == 0:
     print('They are both divisible by 3')
 elif numone % 3 == 0:
     print('Only the first number is divisible by three')
@@ -21,3 +21,9 @@ elif numtwo % 3 == 0:
     print('Only the second number is divisible by three')
 else:
     print('Neither number is divisible by 3')
+
+product = int(input('What is the product of your two numbers?: '))
+if product == numone*numtwo:
+    print('correct')
+else:
+    print('incorrect')
