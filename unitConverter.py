d7728031c75cb419b986660aef5e1b95b069a97a
maxmyth01 <maxmyth01@gmail.com>
2017-09-29 14:30:01 -0400

    Updated from Brython Server: 9/29/2017 2:30:00 PM

--- unitConverter.py
+++ unitConverter.py
@@ -2,8 +2,9 @@
 #9-14-17
 #unitConverter.py - choose a converter
 
-converter = int(input('1) Kilometer to Miles, 2) Kilograms to Pounds, 3) Liters to Gallons, 4)Celsius to Fahrenheit'))
 while True:
+    converter = int(input('1) Kilometer to Miles, 2) Kilograms to Pounds, 3) Liters to Gallons, 4)Celsius to Fahrenheit'))
+
     if converter < 0 or converter > 4:
         print('Error Invalid Converter Number')
 
@@ -15,10 +16,10 @@ while True:
         kg_to_lbs = float(input('Enter a mass in Kilograms'))
         print(kg_to_lbs,'kilograms is', kg_to_lbs*2.20462 ,'pounds')
     
-if converter == 3:
+    if converter == 3:
         L_to_G = float(input('Enter a volume in liters'))
         print(L_to_G,'liters is', L_to_G*0.264172 ,'gallons')
     
-if converter == 4:
+    if converter == 4:
         C_to_F = float(input('Enter a tempature in celsius'))
         print(C_to_F,'degrees celsius is', (C_to_F*1.8)+32 ,'degrees Fahrenheit')
